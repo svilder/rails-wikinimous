bb8b366ab4811f2756a8767992c84c758325fe61
Sandra Vilder <sandra.vilder@gmail.com>
2019-07-11 12:42:50 +0200

    initialized Index and Show, working on basic style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'start seeding'
+Article.destroy_all
+
+Article.create(title: 'The meaning of life', content: "It's 42!")
+
+def create_seed(number_of_times)
+  number_of_times.times do
+    Article.create(title: Faker::Book.title, content: Faker::GreekPhilosophers.quote)
+  end
+end
+
+create_seed(15)
+
+puts 'Done seeding'
